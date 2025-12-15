eb8cefd9a133acdc77113dd095f74407db0ed193
Ueverson Moreira <staltersalime@gmail.com>
2025-12-15 23:04:29 +0000

    adiciona `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 function Home() {
-  return <h1>Consertando a casa... Pela terceira vez</h1>;
+  return (
+    <h1>
+      Mensagem para minha esposa Daiane Rocha. Muito obrigado por acreditar em
+      mim meu amor, eu te amo infinitamente..
+    </h1>
+  );
 }
 
 export default Home;
-
-function teste2() {
-  console.log("indentação errada");
-}
