c216bee03ada0a5b2f016831f9191f94e4017dd3
Ueverson Moreira <staltersalime@gmail.com>
2026-01-13 21:02:34 +0000

    install `jest` and configure test scripts

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 function Home() {
   return (
-    <h1>
-      Mensagem para minha esposa Daiane Rocha. Muito obrigado por acreditar em
-      mim meu amor, eu te amo infinitamente..
-    </h1>
+    <h1>Mensagem para minha esposa Daiane Rocha. Muito obrigado por acreditar em
+      mim meu amor, eu te amo infinitamente.. <3</h1>
   );
 }
 
